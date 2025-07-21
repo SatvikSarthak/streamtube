cea608cef5d113e89964db02dab5807cda593811
SatvikSarthak <satvik.sarthak17@gmail.com>
2025-07-21 22:57:26 +0530

    connection to mongo DB

--- src/db/index.js
+++ src/db/index.js
@@ -5,6 +5,7 @@ const connectDB = async () => {
   try {
   const connectionInstance =  await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
   console.log(`\n MONGO DB CONNECTION HOST IS ${connectionInstance.connection}`);
+  console.log(connectionInstance.connection)
     
   } catch (error) {
     console.error("error", error);
