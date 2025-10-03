3fa34ca325d8c87f6a5c8ec2c3f3b19b67ba45a9
SatvikSarthak <satvik.sarthak17@gmail.com>
2025-10-03 18:46:05 +0530

    Add health check route: integrate health check endpoint for API status monitoring

--- src/app.js
+++ src/app.js
@@ -28,6 +28,7 @@ import subscriptionRoute from "./routes/subscription.routes.js";
 import playlistRoute from "./routes/playlist.routes.js";
 import commentRoute from "./routes/comment.routes.js";
 import likeRoute from "./routes/like.routes.js";
+import healthRoute from "./routes/healthcheck.routes.js";
 // routes declaration
 app.use("/api/v1/users", userRoute);
 app.use("/api/v1/tweet", tweetRoute);
@@ -36,4 +37,5 @@ app.use("/api/v1/subscription", subscriptionRoute);
 app.use("/api/v1/playlist", playlistRoute);
 app.use("/api/v1/comment", commentRoute);
 app.use("/api/v1/like", likeRoute);
+app.use("/api/v1/health", healthRoute);
 export { app };
